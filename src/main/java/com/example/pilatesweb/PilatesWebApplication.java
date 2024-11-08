702c9522ea95d0f8391377fd1b23e82d07718860
Egorov Ilya <i.egorov@astondevs.ru>
2024-11-08 11:11:47 +0300

    Initial commit

---
+++ src/main/java/com/example/pilatesweb/PilatesWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.pilatesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilatesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilatesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pilatesweb/PilatesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pilatesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
